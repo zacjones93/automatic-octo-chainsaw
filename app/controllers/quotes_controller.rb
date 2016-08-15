3ea3b8faf093b7f9e41b59be9ea4caf534e2da04
Zac Jones <zacjones93@gmail.com>
2016-08-15 01:52:33 +0000

    'Contribute' now pushes quotes into the db

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,18 @@
 class QuotesController < ApplicationController
-def index
-  @quote = Quote.order("RANDOM()").first
-end
-def new
-  @quote = Quote.new
-end
+  def index
+    @quote = Quote.order("RANDOM()").first
+  end
+  def new
+    @quote = Quote.new
+  end
+  def create
+      Quote.create(quote_params)
+      redirect_to root_path
+  end
 
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
