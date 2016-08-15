407bca5b3cf91158a1f21541e10762eb45091c1d
Zac Jones <zacjones93@gmail.com>
2016-08-15 02:14:51 +0000

    Added user validation! cool.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,7 +6,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
   def create
-      Quote.create(quote_params)
+      @quote = Quote.create(quote_params)
+      if @quote.invalid?
+        flash[:error] = '<strong> Could not save</strong> the data you entered is invalid.'
+      end
       redirect_to root_path
   end
 
